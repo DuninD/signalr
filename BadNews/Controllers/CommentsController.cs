11d7cb4a30d77b746a48ca46f8fd8e8446115d21
dunin <dunin@skbkontur.ru>
2024-12-03 15:48:45 +0500

    done

---
+++ BadNews/Controllers/CommentsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using BadNews.Models.Comments;
+using BadNews.Repositories.Comments;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BadNews.Controllers
+{
+    [ApiController]
+    public class CommentsController : ControllerBase
+    {
+        private readonly CommentsRepository commentsRepository;
+
+        public CommentsController(CommentsRepository commentsRepository)
+        {
+            this.commentsRepository = commentsRepository;
+        }
+
+        // GET
+        [HttpGet("api/news/{id}/comments")]
+        public ActionResult<CommentsDto> GetCommentsForNews(Guid newsId)
+            => Ok(commentsRepository.GetComments(newsId));
+    }
+}
